929b3ecc8bd3e0075e2f91b5bef5e304b90d8ad4
Abhirup Subba <Abhirup Subba@Devi.attlocal.net>
2018-12-20 14:08:28 -0600

    First commit

---
+++ BestBuyMavenArtiFact/src/test/java/www/BestBuy/com/BestBuy.java
@@ -0,0 +1,41 @@
+package www.BestBuy.com;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+                import org.openqa.selenium.WebDriver;
+                import org.openqa.selenium.WebElement;
+                import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BestBuy {
+
+        public static void main(String[] args) {
+
+ /*               System.setProperty("webdriver.gecko.driver","C:\\Users\\Abhirup Subba\\Downloads\\geckodriver-v0.23.0-win64\\geckodriver.exe");
+                WebDriver driver = new FirefoxDriver();
+
+                 // launch Fire fox and direct it to the Base URL
+        driver.get("https://www.target.com/");
+
+                             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                                driver.close();
+*/
+
+
+        }
+        static void Test() {
+        	
+            System.setProperty("webdriver.gecko.driver","C:\\Users\\Abhirup Subba\\Downloads\\geckodriver-v0.23.0-win64\\geckodriver.exe");
+            WebDriver driver = new FirefoxDriver();
+
+             // launch Fire fox and direct it to the Base URL
+    driver.get("https://www.target.com/");
+
+                         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                            driver.close();
+
+        }
+
+}
---
+++ BestBuyMavenArtiFact/src/test/java/www/BestBuy/com/BestBuyTest.java
@@ -0,0 +1,19 @@
+package www.BestBuy.com;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BestBuyTest {
+
+	@Test
+	public void test() {
+		
+		BestBuy valu = new BestBuy();
+		
+		valu.Test();
+		
+		
+	}
+
+}
