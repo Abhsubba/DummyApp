40f12d5ba507369531ccf3880ae5b7ac0a1711a0
Abhirup Subba <Abhirup Subba@Devi.attlocal.net>
2018-12-20 17:45:19 -0600

    sdds

--- BestBuyMavenArtiFact/src/test/java/www/BestBuy/com/BestBuy.java
+++ BestBuyMavenArtiFact/src/test/java/www/BestBuy/com/BestBuy.java
@@ -13,7 +13,7 @@ public class BestBuy {
 
         public static void main(String[] args) {
 
- /*               System.setProperty("webdriver.gecko.driver","/home/abhirup/.jenkins/workspace/BestBuyAuto/BestBuy/BestBuyMavenArtiFact");
+ /*               System.setProperty("webdriver.gecko.driver","C:\Users\Abhirup Subba\Downloads\geckodriver-v0.23.0-win64\geckodriver.exe");
                 WebDriver driver = new FirefoxDriver();
 
                  // launch Fire fox and direct it to the Base URL
@@ -27,7 +27,7 @@ public class BestBuy {
         }
         static void Test() {
         	
-            System.setProperty("webdriver.gecko.driver","/home/abhirup/.jenkins/workspace/BestBuyAuto/BestBuy/BestBuyMavenArtiFact/geckodriver");
+            System.setProperty("webdriver.gecko.driver","C:\\Users\\Abhirup Subba\\Downloads\\geckodriver-v0.23.0-win64\\geckodriver.exe");
             WebDriver driver = new FirefoxDriver();
 
              // launch Fire fox and direct it to the Base URL
